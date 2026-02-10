bd621ef15b8011cca9e8c01961a04d62193e5701
vlad <vlandgor@gmail.com>
2026-02-10 16:22:42 -0500

    Added home panel UI logic

--- Assets/Scripts/UI/Panels/Menu/Home/HomePanel.cs
+++ Assets/Scripts/UI/Panels/Menu/Home/HomePanel.cs
@@ -7,22 +7,38 @@ namespace UI.Panels.Menu.Home
 {
     public class HomePanel : BasePanel
     {
+        [SerializeField] private Button _customLobbyButton;
         [SerializeField] private Button _playButton;
+        [SerializeField] private Button _gameModesButton;
 
         private void Start()
         {
             _playButton.onClick.AddListener(HandlePlayButtonClicked);
+            _customLobbyButton.onClick.AddListener(HandleCustomLobbyButtonClicked);
+            _gameModesButton.onClick.AddListener(HandleGameModesButtonClicked);
         }
 
         private void OnDestroy()
         {
             _playButton.onClick.RemoveListener(HandlePlayButtonClicked);
+            _customLobbyButton.onClick.RemoveListener(HandleCustomLobbyButtonClicked);
+            _gameModesButton.onClick.RemoveListener(HandleGameModesButtonClicked);
+        }
+        
+        private void HandleCustomLobbyButtonClicked()
+        {
+            
         }
 
         private async void HandlePlayButtonClicked()
         {
             _playButton.interactable = false;
             await MatchmakingService.Instance.PlayAsync();
+        }
+        
+        private void HandleGameModesButtonClicked()
+        {
+            
         }
     }
 }
