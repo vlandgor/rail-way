d454d03979adf61bac062bdff6cb72ba960c73d4
vlad <vlandgor@gmail.com>
2026-02-28 11:48:16 -0500

    Added not MonoBehaviour singleton

---
+++ Assets/Scripts/Utilities/MonoSingleton.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        private static readonly object _lock = new();
+        private static bool _quitting;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_quitting)
+                    return null;
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = FindObjectOfType<T>();
+
+                        if (_instance == null)
+                        {
+                            T prefab = Resources.Load<T>(typeof(T).Name);
+                            if (prefab != null)
+                            {
+                                GameObject instance = Instantiate(prefab.gameObject);
+                                _instance = instance.GetComponent<T>();
+                                DontDestroyOnLoad(instance);
+                            }
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                
+                transform.SetParent(null);
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            _quitting = true;
+            _instance = null;
+        }
+    }
+}
